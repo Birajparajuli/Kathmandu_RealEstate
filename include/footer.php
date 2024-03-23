4b92d34d5e05c72d5217e4b6bd6dd1a3d824a0b1
Biraj <birajparajuli2001@gmail.com>
2024-03-23 15:18:25 +0545

    init

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div>
+                            <h5>RealEstate Nepal</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
